899209c5fc397963ffacda1ea5eb3d517e7e998c
dharmesh_chowdhary <dharmeshchowdhary2000@gmail.com>
2023-02-19 19:25:40 +0530

    Home page & half inbox page

---
+++ index.js
@@ -0,0 +1,57 @@
+myheader.innerHTML = `
+    <div class = "navbar">
+        <div class="logo"></div>
+        <div class="brand"><a>Messenger</a></div>
+    </div>
+`;
+myfooter.innerHTML = `
+    <div class="dock">
+        <div class="docsubdiv-1">
+            <a href = "home.html" class="dock-element">
+                <img src="Extras/home_FILL0_wght400_GRAD0_opsz48.svg" alt="" class="dock-element-img">
+                <h2 class="dock-element-h2">Home</h2>
+            </a>
+        </div>
+
+        <div class="docsubdiv-2">
+            <a href = "inbox.html" class="dock-element"> 
+                <img src="Extras/all_inbox_FILL0_wght400_GRAD0_opsz48.svg" alt="" class="dock-element-img">
+                <h2 class="inbox dock-element-h2">Inbox</h2>
+            </a>
+            <a href = "inbox.html" class="dock-element">
+                <img src="Extras/groups_FILL0_wght400_GRAD0_opsz48.svg" alt="" class="dock-element-img">
+                <h2 class="teams dock-element-h2">Teams</h2>
+            </a>
+            <a href = "inbox.html" class="dock-element">
+                <img src="Extras/library_books_FILL0_wght400_GRAD0_opsz48.svg" class="dock-element-img" alt="">
+                <h2 class="tutorial dock-element-h2">Tutorial</h2>
+            </a>
+        </div>
+        
+        <div class="docsubdiv-3">
+            <a href = "inbox.html" class="dock-element">
+                <img src="Extras/smart_toy_FILL0_wght400_GRAD0_opsz48.svg" class="dock-element-img" alt="">
+                <h2 class="bot dock-element-h2">Bot</h2>
+            </a>
+            <a href = "inbox.html" class="dock-element">
+                <img src="Extras/contacts_FILL0_wght400_GRAD0_opsz48.svg"  class="dock-element-img" alt="">
+                <h2 class="contacts dock-element-h2">Contacts</h2>
+            </a>
+            <a href = "inbox.html" class="dock-element">
+                <img src="Extras/admin_panel_settings_FILL0_wght400_GRAD0_opsz48.svg" class="dock-element-img" alt="">
+                <h2 class="operator dock-element-h2">Operator</h2>
+            </a>
+        </div>
+
+        <div class="docsubdiv-4">
+            <a href = "inbox.html" class="dock-element">
+                <img src="Extras/dashboard_FILL0_wght400_GRAD0_opsz48.svg" class="dock-element-img" alt="">
+                <h2 class="dashboard dock-element-h2">Dashboard</h2>
+            </a>
+            <a href = "inbox.html" class="dock-element">
+                <img src="Extras/account_circle_FILL0_wght400_GRAD0_opsz48.svg" class="dock-element-img" alt="">
+                <h2 class="profile dock-element-h2">Profile</h2>
+            </a>
+        </div>
+    </div>
+`;
